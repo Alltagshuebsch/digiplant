4e207aff17c9a774bb63930ca6f6a8a0ff1de404
alltagshuebsch <privat@alltagshuebsch.de>
2025-05-20 09:38:15 +0200

    Entwurfsstand

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,183 @@
 package org.example;
 
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    /**
+     * TODO Werte Identifizieren
+     * */
+    private static final double MAXBODEN = 1;
+    private static final double MINBODEN = 0.5;
+    private static final double MAXLICHT = 0.7;
+    private static final double MINLICHT = 0.5;
+    private static final double MAXTEMP = 0.7;
+    private static final double MINTEMP = 0.5;
+    private static final int SLEEPTIME = 5000; //5000 Millisekunden = 5 Sekunden
+
+    public static void main(String[] args) {
+        Werte werte;
+
+        while (true){
+        try {
+            Thread.sleep(SLEEPTIME);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        werte = leseSensorWerte();
+        updateDB(prepareInsertWerteInDB(werte)); //nimmt die Werte, implementiert sie in einen Insert-Statement-String und führt aus
+        verarbeiteWerte(werte);
+
+       }
+
+    }
+
+    private static void verarbeiteWerte(Werte werte) {
+        String sqlstmt;
+        int id = getWertIdFromDB(werte);
+        int counter = 0;
+        if (getboden() < MINBODEN){
+            sqlstmt = "INSERT INTO eventAktion (werteId, eventId, aktionId) VALUES (" +id+ ", " +2+ ", " +1+ ");";
+            updateDB(sqlstmt);
+            actionRedLED();
+            counter++;
+        } else if (getboden() > MAXBODEN){
+            sqlstmt = "INSERT INTO eventAktion (werteId, eventId, aktionId) VALUES (" +id+ ", " +1+ ", " +1+ ");";
+            updateDB(sqlstmt);
+            actionRedLED();
+            counter++;
+        }
+
+        if (getLicht() < MINLICHT){
+            sqlstmt = "INSERT INTO eventAktion (werteId, eventId) VALUES (" +id+ ", " +4+ ");";
+            updateDB(sqlstmt);
+            counter++;
+        } else if (getLicht() > MAXLICHT){
+            sqlstmt = "INSERT INTO eventAktion (werteId, eventId) VALUES (" +id+ ", " +3+ ");";
+            updateDB(sqlstmt);
+            counter++;
+        }
+
+        if (getTemp() < MINTEMP){
+            sqlstmt = "INSERT INTO eventAktion (werteId, eventId) VALUES (" +id+ ", " +6+ ");";
+            updateDB(sqlstmt);
+            counter++;
+        } else if (getTemp() > MAXTEMP){
+            sqlstmt = "INSERT INTO eventAktion (werteId, eventId) VALUES (" +id+ ", " +5+ ");";
+            updateDB(sqlstmt);
+            counter++;
+        }
+
+        if (counter == 3){
+            actionRedLED();
+            sqlstmt = "UPDATE eventAktion SET aktionId = 1 WHERE werteId = " +id+";";
+            updateDB(sqlstmt);
+        }else if (counter > 0){
+            actionYellowLED();
+            sqlstmt = "UPDATE eventAktion SET aktionId = 6 WHERE werteId = " +id+";";
+            updateDB(sqlstmt);
+        }else if (counter == 0){
+            actionGreenLED();
+            sqlstmt = "INSERT INTO eventAktion (werteId, eventId, aktionId) VALUES (" +id+ ",7 ,2 );";
+            updateDB(sqlstmt);
+        }
+
+        displayAusgabe();
+    }
+
+    /**
+     * TODO
+     * */
+    private static void displayAusgabe() {
+    }
+
+
+    /**
+     * TODO
+     * */
+    private static void actionGreenLED() {
+    }
+
+    /**
+     * TODO
+     * */
+    private static void actionYellowLED() {
+    }
+
+    /**
+     * TODO
+     * */
+    private static void actionRedLED() {
+    }
+
+
+    private static Werte leseSensorWerte(){
+        return new Werte (getTemp(), getLicht(), getboden());
+    }
+    
+    /**
+     * TODO
+     * */
+    private static float getboden() {
+        return 1;
+    }
+
+    /**
+     * TODO
+     * */
+    private static float getLicht() {
+        return 1;
+    }
+
+    /**
+     * TODO
+     * */
+    private static float getTemp() {
+        return 1;
+    }
+
+    public static String prepareInsertWerteInDB(Werte werte) {
+        return "INSERT INTO werte (zeitpunkt, temperatur, licht, bodenfeuchte) VALUES " +
+                    "('" +werte.getZeitpunkt()+ "', " +werte.getTemperatur()+ ", " +werte.getLicht()+ ", " +werte.getBodenfeuchte()+");";
+    }
+
+
+    public static void updateDB(String preparedString){
+        String connectionUrl = "jdbc:sqlite:src/main/resources/digiplant.db";
+        try (var conn = DriverManager.getConnection(connectionUrl)) {
+            System.out.println("Connection to SQLite has been established.");
+            conn.prepareStatement(preparedString)
+                    .executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+
+    public static int getWertIdFromDB(Werte werte) {
+        String preparedString = "SELECT id FROM werte WHERE zeitpunkt = ?";
+        String connectionUrl = "jdbc:sqlite:src/main/resources/digiplant.db";
+
+        try (var conn = DriverManager.getConnection(connectionUrl);
+             var stmt = conn.prepareStatement(preparedString)) {
+
+            stmt.setString(1, werte.getZeitpunkt());
+            try (var rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    return rs.getInt(1);
+                } else {
+                    throw new SQLException("Kein Wert gefunden!");
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return -1; // Oder eine andere sinnvolle Fehlerbehandlung
         }
     }
 }
--- src/main/java/org/example/Werte.java
+++ src/main/java/org/example/Werte.java
@@ -1,4 +1,55 @@
 package org.example;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Werte {
+    private String zeitpunkt;
+    private float temperatur;
+    private float licht;
+    private float bodenfeuchte;
+
+    public Werte(float temperatur, float licht, float bodenfeuchte) {
+        this.zeitpunkt = getTimeStamp();
+        this.temperatur = temperatur;
+        this.licht = licht;
+        this.bodenfeuchte = bodenfeuchte;
+    }
+
+    public String getZeitpunkt() {
+        return zeitpunkt;
+    }
+
+    public float getTemperatur() {
+        return temperatur;
+    }
+
+    public void setTemperatur(float temperatur) {
+        this.temperatur = temperatur;
+    }
+
+    public float getLicht() {
+        return licht;
+    }
+
+    public void setLicht(float licht) {
+        this.licht = licht;
+    }
+
+    public float getBodenfeuchte() {
+        return bodenfeuchte;
+    }
+
+    public void setBodenfeuchte(float bodenfeuchte) {
+        this.bodenfeuchte = bodenfeuchte;
+    }
+
+    /**
+     *TODO prüfen
+     * */
+    private String getTimeStamp() {
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-YYYY-h-mm-ss");
+        return sdf.format(date);
+    }
 }
